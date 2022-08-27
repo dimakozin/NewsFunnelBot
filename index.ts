7738bf03310ee32089f5e3e6f59f0ce694876a6b
d.kozin <d.kozin@life-pay.ru>
2022-08-27 23:48:17 +0300

    Настроил TS, проброс токена из settings.json

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
+import TelegramBot from 'node-telegram-bot-api';
+import * as settings from './settings.json' 
 
+const token = settings.token;
+const bot = new TelegramBot(token, {polling: true});
 
+bot.on('message', (msg: any) => {
+    // TODO: 
+})
+
+bot.on("polling_error", console.error);
